335314f3ba4511b763e026fb934e01a5ebdce167
checkiamsiam <issiam02415@gmail.com>
2022-04-04 22:31:55 +0600

    about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h1 className='md:text-6xl text-4xl p-20 text-red-300'>Page is Under construction</h1>
     </div>
   );
 };

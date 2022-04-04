afc3e0a5fef635dcc0972325b50ca92a06ef42f4
checkiamsiam <issiam02415@gmail.com>
2022-04-04 22:53:43 +0600

    problems solved

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ReviewCard = ({ data }) => {
   return (
     <div className='md:w-1/2'>
-      <div className="max-w-md w-full md:max-w-full md:flex md:m-10 mb-3">
+      <div className="max-w-md w-full md:max-w-full md:flex md:m-5 mb-3">
         <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
           <div className="mb-8">
             <p className="text-sm text-gray-600 flex items-center">
